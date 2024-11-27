562407d0f097e3931df493db7c90b99121bc7234
GabryMark06 <gabriele.marchesini@alessandrinimainardi.edu.it>
2024-11-27 11:37:17 +0100

    a

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from prima_app.views import homepage,welcome,lista,chi_siamo,variabili,index
-from primo_progetto.views import index_root
+ 
 
 app_name="prima_app"
 urlpatterns=[
@@ -9,6 +9,5 @@ urlpatterns=[
     path('lista',lista,name='lista'),
     path('chi_siamo',chi_siamo,name='chi_siamo'),
     path('variabili',variabili,name='variabili'),
-    path('index_root',index_root,name='index_root'),
     path('',index,name='index')
 ]
