be16631d7890cac6234206563c1ecf2fabdcb891
GabryMark06 <gabriele.marchesini@alessandrinimainardi.edu.it>
2025-03-26 11:58:24 +0100

    23
    23

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import contatti, lista_contatti
+from .views import contatti, lista_contatti, modifica_contatto, elimina_contatto
 
 app_name='forms_app'
 
 urlpatterns = [
     path('contattaci/', contatti, name='contatti'),
     path("lista_contatti/", lista_contatti.as_view(), name="lista_contatti"),
+    path('elimina_contatto/', elimina_contatto, name='elimina-contatto'),
+    path('modifica_contatto/', modifica_contatto, name='modifica-contatto'),
 ]
--- forms_app/views.py
+++ forms_app/views.py
@@ -3,6 +3,11 @@ from .forms import FormContatto
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from .models import Contatto
+from django.shortcuts  import get_object_or_404,redirect
+
+from django.contrib.admin.views.decorators import staff_member_required
+
+from django.contrib.auth.decorators import login_required
 
 def contatti(request):
     form=FormContatto()
@@ -42,6 +47,48 @@ class lista_contatti(ListView):
     template_name = "lista_contatti.html"
     context_object_name = "contatti" 
 
+
+@login_required(login_url='/accounts/login/')
+def modifica_contatto(request, pk):
+    # Preleva dal database l'oggetto la cui chiave primaria è passata come parametro
+    contatto = get_object_or_404(Contatto, id=pk)
+
+    """
+    Se l'oggetto non viene trovato, get_object_or_404 restituisce una pagina di errore HTTP 404 (pagina non trovata).
+    
+    In Django, ci sono principalmente due tipi di richieste HTTP che una view può gestire: GET e POST.
+    - Le richieste GET sono utilizzate per recuperare dati dal server,
+    - Mentre le richieste POST sono utilizzate per inviare dati al server,
+      ad esempio quando si invia un modulo HTML come in questo caso.
+    """
+
+    if request.method == "GET":
+        # Prima chiamata GET per caricare il form
+        form = FormContatto(instance=contatto)  # Al costruttore del form passo il contatto prelevato dal DB
+    elif request.method == "POST":
+        # Seconda chiamata POST per modificare il contatto
+        form = FormContatto(request.POST, instance=contatto)  # Passo al form i dati modificati
+        if form.is_valid():
+            form.save()
+            return redirect('forms_app:lista-contatti')  # URL che reindirizza alla pagina lista_contatti.html
+
+    context = {'form': form, 'contatto': contatto}
+    return render(request, 'modifica_contatto.html', context)
+
+
+
+# Decoratore che permette di cancellare il contatto solo ad un utente admin
+@staff_member_required(login_url='/accounts/login/')
+def elimina_contatto(request, pk):
+    contatto = get_object_or_404(Contatto, id=pk)
+
+    if request.method == "POST":
+        # Vuol dire che l'utente ha inviato il form che conferma l'eliminazione
+        contatto.delete()  # Elimina il contatto dal database
+        return redirect('forms_app:lista-contatti')
+
+    context = {'contatto': contatto}
+    return render(request, 'elimina_contatto.html', context)
 
 
 
